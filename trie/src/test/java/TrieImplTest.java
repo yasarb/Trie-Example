f42e1095017b2bd8b2d06e73f0b70459ad51c15b
yasar budulgan <yb.budulgan@gmail.com>
2017-07-03 23:21:50 +0300

    Add unit test

--- trie/src/test/java/TrieImplTest.java
+++ trie/src/test/java/TrieImplTest.java
@@ -1,6 +1,9 @@
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by ysrbdlgn on 03-Jul-17.
@@ -46,17 +49,80 @@ public class TrieImplTest {
     public void insert() throws Exception {
 
         Trie trie = new TrieImpl();
-        trie.insert("ysrbdlgn");
+        trie.insert("computer");
 
         Assert.assertEquals(trie.isEmpty(), false);
         Assert.assertEquals(trie.wordCount(), 1);
-        Assert.assertEquals(trie.contains("ysrbdlgn"), true);
+        Assert.assertEquals(trie.contains("computer"), true);
 
+        trie.insert("computing");
+        Assert.assertEquals(trie.isEmpty(), false);
+        Assert.assertEquals(trie.wordCount(), 2);
+        Assert.assertEquals(trie.contains("computing"), true);
+
+        trie.insert("garden");
+        Assert.assertEquals(trie.isEmpty(), false);
+        Assert.assertEquals(trie.wordCount(), 3);
+        Assert.assertEquals(trie.contains("garden"), true);
+
+        trie.insert("compute");
+        Assert.assertEquals(trie.contains("compute"), true);
 
     }
 
     @Test
     public void startsWith() throws Exception {
+
+        Trie trie = new TrieImpl();
+        trie.insert("trace");
+        trie.insert("tracing");
+        trie.insert("traceback");
+        trie.insert("track");
+        trie.insert("tracking");
+        trie.insert("trackback");
+
+        Assert.assertEquals(equalLists(trie.startsWith("trace"), new ArrayList<String>() {{
+            add("trace");
+            add("traceback");
+        }}), true);
+
+        Assert.assertEquals(equalLists(trie.startsWith("track"), new ArrayList<String>() {{
+            add("track");
+            add("tracking");
+            add("trackback");
+        }}), true);
+
+        Assert.assertEquals(equalLists(trie.startsWith("trac"), new ArrayList<String>() {{
+            add("trace");
+            add("tracing");
+            add("traceback");
+            add("track");
+            add("tracking");
+            add("trackback");
+        }}), true);
+
+
+    }
+
+    private boolean equalLists(List<String> one, List<String> two) {
+        // https://stackoverflow.com/a/13501200
+
+        if (one == null && two == null) {
+            return true;
+        }
+
+        if ((one == null && two != null)
+                || one != null && two == null
+                || one.size() != two.size()) {
+            return false;
+        }
+
+        one = new ArrayList<String>(one);
+        two = new ArrayList<String>(two);
+
+        Collections.sort(one);
+        Collections.sort(two);
+        return one.equals(two);
     }
 
 }
